f0471374e804bbab38453c796b8ced2775ddb0cf
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-04 14:31:46 +0530

    solving power of 2 problem using recursion

---
+++ DSA/Recursion/power.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std; 
+
+int power(int a, int b) {
+    if(a == 0)
+       return 1;
+
+    if(b == 1)
+      return a;
+
+    int ans = power(a, b / 2);
+
+    if(b % 2 == 0) {
+        return ans * ans;
+    } else {
+        return a * ans * ans;
+    }     
+} 
+
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    int ans = power(a, b);
+    cout<<ans<<endl;
+    return 0;
+}
