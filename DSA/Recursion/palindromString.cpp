344ebf216eae3fba37b1908968a9c8bef807d2da
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-04 14:23:26 +0530

    using only single value

--- DSA/Recursion/palindromString.cpp
+++ DSA/Recursion/palindromString.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-bool checkPalindrome(string str, int i, int j)
+bool checkPalindrome(string str, int i)
 {
-    if (i > j)
+    int j = str.length();
+    if (i > j / 2)
         return true;
 
-    if (str[i] != str[j])
+    if (str[i] != str[j-1-i])
         return false;
-
+        
     else
-        return checkPalindrome(str, i + 1, j - 1);
+        return checkPalindrome(str, i + 1);
 }
 
 int main()
@@ -18,7 +19,7 @@ int main()
     string str = "abaaba";
     cout << endl;
 
-    bool isPalidrome = checkPalindrome(str, 0, str.length()-1);
+    bool isPalidrome = checkPalindrome(str, 0);
     if(isPalidrome) {
         cout<<"String is Palindrome"<<endl;
     } else {
